0eddfc4795bd60b507d891bffb490631a67f1af3
yeonggyulim <lyg940128@gmail.com>
2020-11-04 21:36:11 +0900

    [webpack-loader] add several loaders

---
+++ webpack-loader/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.txt$/,
+        use: "raw-loader",
+      },
+    ],
+  },
+  mode: "production",
+};
